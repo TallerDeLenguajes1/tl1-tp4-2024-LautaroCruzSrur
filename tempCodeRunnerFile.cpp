2b3d1c32558983edcca3f03e8313349f89c77ad0
Lautaro G Cruz Srur <105937766+LautaroCruzSrur@users.noreply.github.com>
2024-05-01 17:12:03 -0300

    realizando tareas

--- tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
-void InsertarTarea2(Nodo *Star, Nodo *NuevoNodo){ //FUNCION PARA INTRODUCIR AL FINAL DE LA LISTA
-    Nodo *Aux= Star;
-    while(Aux->Siguiente){
-        Aux = Aux->Siguiente;
-    }
-    Aux-> Siguiente = NuevoNodo;
-}
